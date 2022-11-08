fc27ea42144c528688c2dab4fdf26460f99ef0bb
Estefania Tobajas <estefaniatob@usal.es>
2022-11-08 16:08:26 +0100

    edit new analysis

--- Manuscript/Figures/Plot_1.R
+++ Manuscript/Figures/Plot_1.R
@@ -5,36 +5,54 @@ library(effects)
 library(patchwork)
 
 
-
-# stability data for five plant species present at least in five sites
+# staility of visitation rate and richness and asynchrony
+# plant species present at least in five sites
 # C.crispu, C. ladanifer, C. salviifolius, H.halimifolium and L. pedunculata
 
-data_plant<- read.csv("Data/Stability_fivespecies.csv")
+# stability data 
+data_plant<- read.csv("Data/Stability.csv")
 data_plant<-data_plant[,-1]
 
 
-# Replace inf with NA
-data_plant[sapply(data_plant, is.infinite)] <- NA
+# eliminar plant species 
+sp_out2=data_plant %>%
+  group_by(Plant_gen_sp) %>%
+  summarise(Unique_Id = n_distinct(Site_ID)) %>%
+  filter(Unique_Id <= 4) %>%
+  select(Plant_gen_sp) %>% pull(Plant_gen_sp)
+
+#Data with species in at least 5 sites
+data_plant = data_plant %>% filter(!Plant_gen_sp %in% sp_out2)
+
+levels(factor(data_plant$Plant_gen_sp))
+
+
+
+## Replace Inf and -Inf with NA
+data_plant[is.na(data_plant) | data_plant == "Inf"] <- NA 
+data_plant[is.na(data_plant) | data_plant == "-Inf"] <- NA 
+
+
 
 # We can express asynchrony as 1 - φ, 
 #where φ is Loreau and de Mazancourt’s (2008)
-data_plant$asynchrony= 1-data_plant$syncLM
+data_plant$asyn_LM= 1-data_plant$syncLM
 
 
 #model 
-mod1_sta= lmer (cv_1_visitation ~ S_total + asynchrony + (1|Site_ID) + (1|Plant_gen_sp), data = data_plant)
+mod1_sta= lmer (cv_1_visitation ~ S_total + asyn_LM + (1|Site_ID) + (1|Plant_gen_sp), data = data_plant)
 
 
 
 #getting effects for asynchrony
-effe_mod_sta <-data.frame( effect("asynchrony", mod1_sta, se = TRUE))
+effe_mod_sta <-data.frame( effect("asyn_LM", mod1_sta, se = TRUE))
 
 
 #plot model of asynchrony
-p1=ggplot(effe_mod_sta, aes(asynchrony, fit)) + geom_line(size=1)+
+p1=ggplot(effe_mod_sta, aes(asyn_LM, fit)) + geom_line(size=1)+
   geom_ribbon(aes(ymin = lower, ymax = upper), linetype = 3,alpha=0.1, colour = "black")+
-  scale_y_continuous(limits = c(-0.14, 6))+ labs(color='Plant species')+
-  geom_point(data = data_plant, aes(x =  asynchrony, y = cv_1_visitation, color=Plant_gen_sp), size=3)+
+  scale_y_continuous(limits = c(-2, 9.5))+labs(color='Plant species')+
+  geom_point(data = data_plant, aes(x =  asyn_LM, y = cv_1_visitation, color=Plant_gen_sp), size=3)+
   theme_classic ()+theme(panel.border = element_rect(colour = "black", fill=NA))+
   labs(x = "Asynchrony of pollinator",y="Stability of visitation rate")+
   guides(colour=guide_legend(nrow=2,byrow=TRUE))+ theme(legend.text = element_text(face="italic"))
@@ -49,7 +67,7 @@ effe2_mod_sta <-data.frame( effect("S_total", mod1_sta, se = TRUE))
 #plot model of richness
 p2=ggplot(effe2_mod_sta, aes(S_total, fit)) + geom_line(size=1)+
   geom_ribbon(aes(ymin = lower, ymax = upper), linetype = 3, alpha=0.1, colour = "black")+
-  scale_y_continuous(limits = c(-0.14, 6))+  labs(color='Plant species')+
+  scale_y_continuous(limits = c(-2, 9.5))+  labs(color='Plant species')+
   geom_point(data = data_plant, aes(x =  S_total, y = cv_1_visitation, color=Plant_gen_sp), size=3)+
   theme_classic ()+theme(panel.border = element_rect(colour = "black", fill=NA))+
   labs(x = "Richness of pollinator",y="") +
