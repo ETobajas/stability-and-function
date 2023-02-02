7c56df6822be57bc3ab6bde73e95594166c2a133
Estefania Tobajas <estefaniatob@usal.es>
2023-02-02 13:43:50 +0100

    edit

--- Manuscript/Figures/Conceptual_plot.R
+++ Manuscript/Figures/Conceptual_plot.R
@@ -22,9 +22,9 @@ d3 = data.frame(l3, s3)
 
 
 to=ggplot(d1, aes(s1, l1)) +
-  geom_line(color = "#e41a1c", size=1.3) +
-  geom_line(data =  d2, aes(s2,l2), color = "#377eb8",size=1.3) +
-  geom_line(data =  d3, aes(s3,l3), color = "#4daf4a",size=1.3) +
+  geom_line(color = "#e41a1c", linewidth=1.3) +
+  geom_line(data =  d2, aes(s2,l2), color = "#377eb8",linewidth=1.3) +
+  geom_line(data =  d3, aes(s3,l3), color = "#4daf4a",linewidth=1.3) +
   ylab("Reproductive success")+
   labs(x = "") +
   theme_classic()+
@@ -90,8 +90,8 @@ v_1=ggplot(da2, aes(x2, y2))+
   theme(aspect.ratio=0.5)
 
 
-po1=R_1/v_1 & theme(plot.background = element_rect(color = "black", size = 1))
 
+po1=R_1/v_1& theme(plot.background = element_rect(color = "black", size = 1))
 
 ######################
 x3 <- seq(0, 57, by = 0.1)
@@ -142,13 +142,13 @@ R_2=ggplot(da4, aes(x4, y4))+
   theme(aspect.ratio=0.5)
 
 
-po2=v_2/R_2
+po2=v_2/R_2& theme(plot.background = element_rect(color = "black", size = 1))
 
 ########################################
 
 design <- "
-  11#
-  24#
+  AA#
+  BC#
 "
 
 to+po1+v_2+R_2+ plot_layout(heights  = c(3,1,1),design = design)
@@ -157,3 +157,4 @@ to / ((v_1 / R_1)|(v_2 / R_2))
 
 to/ (po1 |po2)
  
+to - (po1 | po2) + plot_layout(ncol=1)
